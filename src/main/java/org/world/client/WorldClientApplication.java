02c05e8e9451edf6fdf7268a2ddd75a7887a431d
Sathish Parthasarathy <sathish.parthasarathy@sprint.com>
2017-08-07 11:03:03 -0500

    fix the @RibbonClinet issue

--- src/main/java/org/world/client/WorldClientApplication.java
+++ src/main/java/org/world/client/WorldClientApplication.java
@@ -2,10 +2,21 @@ package org.world.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
 @SpringBootApplication
+
 public class WorldClientApplication {
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 	public static void main(String[] args){
 		SpringApplication.run(WorldClientApplication.class, args);
 	}
--- src/main/java/org/world/client/controller/CityController.java
+++ src/main/java/org/world/client/controller/CityController.java
@@ -1,18 +1,33 @@
 package org.world.client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.world.client.service.CityService;
+import org.springframework.web.client.RestTemplate;
+import org.world.config.RibbonConfiguration;
 
 @RestController
+@RibbonClient(name = "worldrs",configuration = RibbonConfiguration.class)
 public class CityController {
 	@Autowired
-	CityService cityService;
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
 	
 	@RequestMapping(value="/city", method = RequestMethod.GET, produces="application/json")
 	public String getAll() {
-		return cityService.getAll();
+		return restTemplate.getForObject("http://worldrs/v2/world/api/city", String.class);
 	}
+	
+	@RequestMapping(value="/cityLoad", method = RequestMethod.GET, produces="application/json")
+	public String getAllLoad() {
+		ServiceInstance serviceInstance = loadBalancerClient.choose("worldrs");
+		return restTemplate.getForObject("http://worldrs/v2/world/api/city", String.class);
+	}
+	
 }
