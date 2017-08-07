8c3e25be02562e106a3a83fcff8127be7a1667ca
Sathish Parthasarathy <sathish.parthasarathy@sprint.com>
2017-08-07 13:51:24 -0500

    fix the @RibbonClient

--- src/main/java/org/world/client/controller/CityController.java
+++ src/main/java/org/world/client/controller/CityController.java
@@ -1,5 +1,8 @@
 package org.world.client.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -8,10 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.world.client.entity.City;
 import org.world.config.RibbonConfiguration;
 
 @RestController
-@RibbonClient(name = "worldrs",configuration = RibbonConfiguration.class)
+@RibbonClient(name = "worldrs", configuration = RibbonConfiguration.class)
 public class CityController {
 	@Autowired
 	private RestTemplate restTemplate;
@@ -20,8 +24,8 @@ public class CityController {
 	private LoadBalancerClient loadBalancerClient;
 	
 	@RequestMapping(value="/city", method = RequestMethod.GET, produces="application/json")
-	public String getAll() {
-		return restTemplate.getForObject("http://worldrs/v2/world/api/city", String.class);
+	public City[] getAll() throws URISyntaxException {
+		return restTemplate.getForObject("http://worldrs/v2/world/api/city",City[].class);	
 	}
 	
 	@RequestMapping(value="/cityLoad", method = RequestMethod.GET, produces="application/json")
@@ -29,5 +33,11 @@ public class CityController {
 		ServiceInstance serviceInstance = loadBalancerClient.choose("worldrs");
 		return restTemplate.getForObject("http://worldrs/v2/world/api/city", String.class);
 	}
+	
+	public static void printUri(URI uri) {
+        System.out.println("toString() --> " + uri.toString());
+        System.out.println("getPath --> " + uri.getPath());
+        System.out.println("getPath null? " + (uri.getPath() == null));
+    }
 	
 }
